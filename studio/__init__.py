d0c4dc37bd42d9900c31efc220f5e51e43c290df
Katelyn <tanner8922@gmail.com>
2026-09-13 18:17:03 -0500

    Expose LLM production interfaces

--- studio/__init__.py
+++ studio/__init__.py
@@ -1,5 +1,15 @@
 """Sasquatch Story Studio core package."""
 
+from .llm import LLMAdapter, LLMPolicy, LLMRequest, LLMResponse, LLMRouter
 from .models import EpisodePlan, ProductionManifest, Story
 
-__all__ = ["EpisodePlan", "ProductionManifest", "Story"]
+__all__ = [
+    "EpisodePlan",
+    "ProductionManifest",
+    "Story",
+    "LLMAdapter",
+    "LLMPolicy",
+    "LLMRequest",
+    "LLMResponse",
+    "LLMRouter",
+]
