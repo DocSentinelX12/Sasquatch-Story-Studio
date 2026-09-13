8b7c9ca1463fed98e822ff6139559edd7b8a8adf
Katelyn <tanner8922@gmail.com>
2026-09-13 18:51:54 -0500

    Expose core production orchestration interfaces

--- studio/__init__.py
+++ studio/__init__.py
@@ -1,18 +1,16 @@
 """Sasquatch Story Studio core package."""
 
+from .checkpoints import CheckpointStore
+from .director import build_episode_plan
 from .interpreter import StoryInterpretation, interpret_story
 from .llm import LLMAdapter, LLMPolicy, LLMRequest, LLMResponse, LLMRouter
 from .models import EpisodePlan, ProductionManifest, Story
+from .production import ProductionRequest, ProductionResponse, ProductionRouter
+from .qc import QCResult, release_ready, validate_episode_plan
 
 __all__ = [
-    "EpisodePlan",
-    "ProductionManifest",
-    "Story",
-    "StoryInterpretation",
-    "interpret_story",
-    "LLMAdapter",
-    "LLMPolicy",
-    "LLMRequest",
-    "LLMResponse",
-    "LLMRouter",
+    "EpisodePlan", "ProductionManifest", "Story", "StoryInterpretation", "interpret_story",
+    "LLMAdapter", "LLMPolicy", "LLMRequest", "LLMResponse", "LLMRouter",
+    "CheckpointStore", "build_episode_plan", "ProductionRequest", "ProductionResponse",
+    "ProductionRouter", "QCResult", "release_ready", "validate_episode_plan",
 ]
