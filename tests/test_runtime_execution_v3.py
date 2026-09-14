507d16ff77639c02cf177a4f50c406a0edafc557
Katelyn <tanner8922@gmail.com>
2026-09-13 22:38:48 -0500

    chore: remove superseded runtime integration drafts

--- tests/test_runtime_execution_v3.py
+++
@@ -1,5 +0,0 @@
-from studio.runtime_execution import RuntimeExecutionCoordinator
-
-
-def test_runtime_execution_coordinator_exposes_evidence_gated_execute():
-    assert callable(RuntimeExecutionCoordinator.execute)
